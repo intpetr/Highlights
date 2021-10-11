455b7e97c87a075894353521722ac2f0d17e4bbf
intpetr <62703463+intpetr@users.noreply.github.com>
2021-10-11 22:17:13 +0200

    Add files via upload

---
+++ Highlights/Highlights/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Highlights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.Location = new System.Drawing.Point(300, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save this session";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(241, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Last saved moment in this session";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label2.Location = new System.Drawing.Point(7, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Session name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "00:00:00";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button2.Location = new System.Drawing.Point(12, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start/Pause";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label4.Location = new System.Drawing.Point(12, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "-";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button3.Location = new System.Drawing.Point(184, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 39);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(477, 226);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Moments";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Highlights/Highlights/Form1.cs
@@ -0,0 +1,193 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Highlights
+{
+    public partial class Form1 : Form
+    {
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+        private string session = "";
+        private string output = "";
+        private Boolean isRunning = false;
+        private int momentNumber = 0;
+       // private Boolean isSaved = false;
+        private Boolean isPaused = false;
+        private Boolean firstStart = false;
+        private IKeyboardMouseEvents m_GlobalHook;
+
+
+
+        private string SessionName = "";
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+           // timer.Start();
+
+            m_GlobalHook = Hook.GlobalEvents();
+            //m_GlobalHook += onGlobalHookEvent();
+
+
+            Hook.GlobalEvents().OnCombination(new Dictionary<Combination, Action>
+{
+
+    {Combination.FromString("Alt+F9"), () => { startSession(); }},
+                
+    {Combination.FromString("Alt+F10"), () => { addMoment(); }}
+});
+
+
+
+
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //mondjal egy szint pls marko pls halo
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = timer.Elapsed.ToString();
+
+            saveMoments();
+
+
+
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Session name: " + textBox1.Text;
+            SessionName = textBox1.Text;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void startSession()
+        {
+            if (isPaused)
+            {
+                label4.Text = "Session On";
+                isPaused = false;
+                timer.Start();
+                isRunning = true;
+            }
+            else
+            {
+                if (!firstStart)
+                {
+                    label4.Text = "Session On";
+                    isPaused = false;
+                    timer.Start();
+                    firstStart = true;
+                    isRunning = true;
+                }
+                else
+                {
+                    label4.Text = "Session Paused";
+                    isPaused = true;
+                    timer.Stop();
+                    
+                }
+
+                
+
+            }
+
+            
+
+        }
+
+
+        private void saveMoments()
+        {
+
+            string allMoments = SessionName+"  Time: "+timer.Elapsed;
+            allMoments = allMoments + "\n" + output;
+            File.WriteAllText(@"C:\Moments\"+SessionName+".txt", allMoments);
+        }
+
+
+
+
+
+
+
+        private void addMoment()
+        {
+            momentNumber++;
+
+            output = output + "\n"+momentNumber+ ": "+timer.Elapsed;
+            label3.Text = timer.Elapsed.ToString();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            startSession();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+
+
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to stop the ongoing session without saving? The data will be lost.", "", MessageBoxButtons.OKCancel);
+                if (dialogResult == DialogResult.OK)
+                {
+
+
+                    timer.Reset();
+                   // timer = new System.Diagnostics.Stopwatch();
+                    output = "";
+                    isRunning = false;
+                    momentNumber = 0;
+                    textBox1.Text = "";
+                    session = "";
+                    label4.Text = "No ongoing session";
+                    label2.Text = "Session name";
+
+                }
+                else if (dialogResult == DialogResult.Cancel)
+                {
+
+                }
+            }
+        }
+
+       
+    }
+}
